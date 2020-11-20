3bc8b59ba7d04741647701cb069e83c90e685378
Ashkan Yadegari <ashkan.yadegari@outlook.com>
2020-11-20 15:04:06 +0800

    Hello World

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,26 +25,26 @@ if Rails.env.development?
     p "#{ingredient.name} has been created"
   end
 
-  puts "Destroying all cocktail  seeds"
-  puts "Starting cocktail seed"
-  Cocktail.destroy_all
+  # puts "Destroying all cocktail  seeds"
+  # puts "Starting cocktail seed"
+  # Cocktail.destroy_all
 
-    url = 'https://tuxedono2.com/recipes/'
-    direct_website = open(url).read
-    nokogiri_object = Nokogiri::HTML(direct_website)
+  #   url = 'https://tuxedono2.com/recipes/'
+  #   direct_website = open(url).read
+  #   nokogiri_object = Nokogiri::HTML(direct_website)
 
-    cocktail_card = nokogiri_object.search(".recipe_admin_element")[0]
+  #   cocktail_card = nokogiri_object.search(".recipe_admin_element")[0]
 
-    cocktail_card.each do |card|
-      image = card.search(".element-image").attr("src").value.gsub("100x100","600x400")
-      file = URI.open(image)
-      title = card.search(".index-element-name").text
-      description = card.search(".subtext").text.strip.gsub("\n",", ")
+  #   cocktail_card.each do |card|
+  #     image = card.search(".element-image").attr("src").value.gsub("100x100","600x400")
+  #     file = URI.open(image)
+  #     title = card.search(".index-element-name").text
+  #     description = card.search(".subtext").text.strip.gsub("\n",", ")
 
-      cocktail = Cocktail.new(name: title, description: description, difficulty: %w(easy medium hard).sample, image: image)
-      cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/png')
-      cocktail.save
-      puts "#{cocktail.name} was added to the db"
-    end
+  #     cocktail = Cocktail.new(name: title, description: description, difficulty: %w(easy medium hard).sample, image: image)
+  #     cocktail.photo.attach(io: file, filename: "#{cocktail.name}.jpg", content_type: 'image/png')
+  #     cocktail.save
+  #     puts "#{cocktail.name} was added to the db"
+  #   end
 
 end
